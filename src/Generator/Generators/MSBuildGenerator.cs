e414f2a527d09512ab82e142aeb4629af1ed64ef
Dimitar Dobrev <dpldobrev@protonmail.com>
2020-12-29 23:25:55 +0200

    Make auto-compiling of C# work with .NET Core
    CodeDom doesn't work in .NET Core (https://github.com/dotnet/runtime/issues/18768) and Roslyn turns out to be too low level (just a compiler and not a building framework) so the best approach is to invoke msbuild as a process.
    
    Signed-off-by: Dimitar Dobrev <dpldobrev@protonmail.com>

---
+++ src/Generator/Generators/MSBuildGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CppSharp.AST;
+
+namespace CppSharp.Generators
+{
+    public class MSBuildGenerator : CodeGenerator
+    {
+        public MSBuildGenerator(Module module, Dictionary<Module, string> libraryMappings, string outputDir)
+            : base(null)
+        {
+            this.module = module;
+            this.libraryMappings = libraryMappings;
+            this.outputDir = outputDir;
+        }
+
+        public override string FileExtension => "csproj";
+
+        public override void Process()
+        {
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            WriteLine($@"
+<Project Sdk=""Microsoft.NET.Sdk"">
+  <PropertyGroup>
+    <TargetFramework>netstandard2.1</TargetFramework>
+    <PlatformTarget>AnyCPU</PlatformTarget>
+    <OutputPath>{outputDir}</OutputPath>
+    <DocumentationFile>{module.LibraryName}.xml</DocumentationFile>
+    <Configuration>Release</Configuration>
+    <AllowUnsafeBlocks>true</AllowUnsafeBlocks>
+    <EnableDefaultNoneItems>false</EnableDefaultNoneItems>
+    <EnableDefaultItems>false</EnableDefaultItems>
+    <AppendTargetFrameworkToOutputPath>false</AppendTargetFrameworkToOutputPath>
+  </PropertyGroup>
+  <ItemGroup>
+    {string.Join(Environment.NewLine,
+         module.CodeFiles.Select(c => $"<Compile Include=\"{c}\" />"))}
+  </ItemGroup>
+  <ItemGroup>
+    {string.Join(Environment.NewLine,
+         new[] { Path.Combine(Path.GetDirectoryName(location), "CppSharp.Runtime.dll") }
+         .Union(from dependency in module.Dependencies
+                where libraryMappings.ContainsKey(dependency)
+                select libraryMappings[dependency])
+         .Select(reference =>
+ $@"<Reference Include=""{Path.GetFileNameWithoutExtension(reference)}"">
+      <HintPath>{reference}</HintPath>
+    </Reference>"))}
+  </ItemGroup>
+</Project>".Trim());
+        }
+
+        private readonly Module module;
+        private readonly Dictionary<Module, string> libraryMappings;
+        private readonly string outputDir;
+    }
+}
